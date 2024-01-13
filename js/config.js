8f4351256f04797c23119e46902aabb2a2f9d50e
jaredesmith <jaredesmith@gmail.com>
2024-01-12 17:04:14 -0800

    initial checkin

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_oMvVEC4Ab', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6e7e325el1elrkknajpfmhmdmd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://wu5hikut3k.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
